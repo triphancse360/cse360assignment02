ce4dca648f8cebdf094af2f8224c717fb0ccaded
Tri Phan <quanprokuteo1998@gmail.com>
2020-10-05 21:00:38 -0700

    Initial commit of the project.

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,42 @@
+package cse360assignment02;
+
+public class AddingMachine {
+/**
+ * instant variable
+ */
+  private int total;
+/**
+ * default constructor
+ */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+/**
+ * method to get total
+ * @return
+ */
+  public int getTotal () {
+    return 0;
+  }
+/**
+ * method to add value to total 
+ * @param value
+ */
+  public void add (int value) {
+  }
+/**
+ * method to subtract value from total
+ * @param value
+ */
+  public void subtract (int value) {
+  }
+/**
+ * method to get history
+ */
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
